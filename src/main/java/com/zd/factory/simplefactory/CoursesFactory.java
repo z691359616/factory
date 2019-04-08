3d6ab75eed9de7600659d96bee754f3db788c51e
z691359616 <691359616@qq.com>
2019-04-08 21:49:22 +0800

    this is factory test

--- src/main/java/com/zd/factory/simplefactory/CoursesFactory.java
+++ src/main/java/com/zd/factory/simplefactory/CoursesFactory.java
@@ -5,12 +5,37 @@ import com.zd.factory.JavaCourses;
 import com.zd.factory.PythonCourses;
 
 public class CoursesFactory {
-    public ICourses create(String name){
-        if("JAVA".equals(name)){
-            return new JavaCourses();
-        }else if("PYTHON".equals(name)){
-            return new PythonCourses();
-        }else
-            return null;
+//    public ICourses create(String name){
+//        if("JAVA".equals(name)){
+//            return new JavaCourses();
+//        }else if("PYTHON".equals(name)){
+//            return new PythonCourses();
+//        }else
+//            return null;
+//    }
+
+//    public ICourses create(String className){
+//        try{
+//            if(null != className && !"".equals(className) ){
+//                return (ICourses) Class.forName(className).newInstance();
+//            }else
+//                return null;
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
+    public ICourses create(Class clazz){
+        try{
+            if(null != clazz ){
+                return (ICourses) clazz.newInstance();
+            }else
+                return null;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
+
 }
--- src/main/java/com/zd/factory/simplefactory/SimpleFactoryTest.java
+++ src/main/java/com/zd/factory/simplefactory/SimpleFactoryTest.java
@@ -1,6 +1,7 @@
 package com.zd.factory.simplefactory;
 
 import com.zd.factory.ICourses;
+import com.zd.factory.PythonCourses;
 
 public class SimpleFactoryTest {
     public static void main(String[] args) {
@@ -8,8 +9,16 @@ public class SimpleFactoryTest {
 //        ICourses iCourses = new JavaCourses();
 //        iCourses.record();
 
+//        CoursesFactory factory = new CoursesFactory();
+//        ICourses iCourses = factory.create("PYTHON");
+//        iCourses.record();
+
+//        CoursesFactory factory = new CoursesFactory();
+//        ICourses iCourses =  factory.create("com.zd.factory.JavaCourses");
+//        iCourses.record();
+
         CoursesFactory factory = new CoursesFactory();
-        ICourses iCourses = factory.create("PYTHON");
+        ICourses iCourses =  factory.create(PythonCourses.class);
         iCourses.record();
 
 
